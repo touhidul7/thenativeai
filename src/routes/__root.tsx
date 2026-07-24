cca2f2636a6de39d11c6a2daac16679e9d892e11
Touhidul Islam Shadhin <116108915+touhidul7@users.noreply.github.com>
2026-07-24 21:08:09 +0600

    Change favicon from favicon.ico to letter-t (1).png

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -104,7 +104,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/letter-t (1).png", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
